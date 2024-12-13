dc73ed1fcda441fd5d4586c2cbadf98b686d1f98
bharath02233 <bharath02233@gmail.com>
2024-12-13 20:40:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:'false'}));
 
-
+app.options('*', cors());
 app.post("/submit",aync(req,res)=>{
    // await quizMod.deleteMany({});
    await quizMod.insertMany(req.body)
